0db6abb96617da522aec16d8ceb375faefd8e51f
dv0 <dv0@dv0.org>
2016-10-04 11:42:53 -0700

    10/4

--- calendar_test.py
+++ calendar_test.py
@@ -8,4 +8,6 @@ Mo, Da, Ye = map(int, raw_input().split())
 
 WDnum = calendar.weekday(Ye, Mo, Da)
 
-print(WDnum)
+DayDict = {0 : 'MONDAY', 1 : 'TUESDAY', 2 : 'WEDNESDAY', 3 : 'THURSDAY', 4 : 'FRIDAY', 5 : 'SATURDAY', 6 : 'SUNDAY'}
+
+print(DayDict[WDnum])
